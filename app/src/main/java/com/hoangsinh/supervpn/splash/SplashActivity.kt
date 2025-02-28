33191038fffc703f03e14054dd832db047c90bd1
HoangSinh <hoangsinh.hhbg@gmail.com>
2025-02-28 23:22:47 +0700

    cm

--- app/src/main/java/com/hoangsinh/supervpn/splash/SplashActivity.kt
+++ app/src/main/java/com/hoangsinh/supervpn/splash/SplashActivity.kt
@@ -25,7 +25,7 @@ class SplashActivity : ProductActivity<ActivitySplashBinding>() {
                 progress += 5
                 binding.customProgressBar.progress = progress
             }
-            Navigator.startMainActivity(SuperVpnApplication.instance)
+            nextScreen()
         }
     }
 
